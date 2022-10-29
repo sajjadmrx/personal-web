dbf775b64b7465329bbf0f2c62ac89ae429b9b57
sajjadmrx <sajjadzibaee102@gmail.com>
2022-10-29 18:41:46 +0330

    added logo & change navbar

--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -1,12 +1,34 @@
 import React from "react";
+import {Navbar} from "flowbite-react";
 
 export class HeaderComponent extends React.Component<any, any> {
     render() {
         return (
-            <header className="flex justify-between items-center h-16 mb-5">
-                <div className="navbar bg-neutral text-neutral-content" dir={"rtl"}>
-                    <a className="btn btn-ghost normal-case text-xl">sajjadmrx</a>
-                </div>
+            <header dir={"ltr"} color={"dark"} className={"mb-5 dark"}>
+                <Navbar
+                    fluid={true}
+                    rounded={true}
+                >
+                    <Navbar.Brand href="https://flowbite.com/">
+                        <img
+                            src="/assets/icons/logo.png"
+                            className="mr-3 h-6 sm:h-9 rounded"
+                            alt="Flowbite Logo"
+                        />
+                        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+      sajjadmrx.ir
+    </span>
+                    </Navbar.Brand>
+                    <Navbar.Toggle/>
+                    <Navbar.Collapse>
+                        <Navbar.Link
+                            href="https://github.com/sajjadmrx/personal-web"
+                        >
+                            لینک سورس سایت
+                        </Navbar.Link>
+
+                    </Navbar.Collapse>
+                </Navbar>
             </header>
         )
     }
