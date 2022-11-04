0e099a5b13662d233bc1b241a692b4aad63b78df
sajjadmrx <sajjadzibaee102@gmail.com>
2022-11-04 20:44:09 +0330

    change font size

--- src/components/biography/index.tsx
+++ src/components/biography/index.tsx
@@ -2,14 +2,21 @@ import React from "react";
 
 export class BiographyComponent extends React.Component<any, any> {
     render() {
+        const aboutMe: string = ``;
         return (
             <div>
                 <div className="mb-10">
                     <div className="flex items-center  dark:text-gray-300 mb-2">
-                        <h2 className="mx-2  font-semibold text-2xl  ">
+                        <h1 className="mx-0  font-semibold text-3xl  ">
                             👋
                             به سایت شخصی من خوش آمدید
-                        </h2>
+                        </h1>
+
+                    </div>
+                    <div className={'mx-6 mt-5 '}>
+                        <p className={'text-sm'}>
+                            {aboutMe}
+                        </p>
                     </div>
                 </div>
             </div>
